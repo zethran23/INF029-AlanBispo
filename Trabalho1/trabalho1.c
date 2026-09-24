c200f85bc3286d244060f58e00f50d03f87c3f8d
zethran23 <106888942+zethran23@users.noreply.github.com>
2026-09-23 21:24:24 -0300

    Preenchi dados

--- Trabalho1/trabalho1.c
+++ Trabalho1/trabalho1.c
@@ -10,10 +10,10 @@
 //  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
 
 //  ----- Dados do Aluno -----
-//  Nome:
-//  email:
-//  Matrícula:
-//  Semestre:
+//  Nome: Alan
+//  email: 20241160037@ifba.edu.br
+//  Matrícula: 20241160037
+//  Semestre: 2026.2
 
 //  Copyright © 2016 Renato Novais. All rights reserved.
 // Última atualização: 07/05/2021 - 19/08/2016 - 17/10/2025
