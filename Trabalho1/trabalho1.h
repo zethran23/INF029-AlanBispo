bca600ba3b19a2780b5fc07938f4b3f9ea5a1524
zethran23 <106888942+zethran23@users.noreply.github.com>
2026-09-23 21:20:00 -0300

    Header do arquivo trabalho1
    Added data structures and function declarations for the project.

--- Trabalho1/trabalho1.h
+++ Trabalho1/trabalho1.h
@@ -1,1 +1,55 @@
 
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome:
+//  email:
+//  Matrícula:
+//  Semestre:
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021
+
+// #################################################
+#ifndef TRABALHO1_H
+#define TRABALHO1_H
+
+typedef struct DQ
+{
+    int iDia; 
+    int iMes;
+    int iAno;
+    int valido; // 0 se inválido, e 1 se válido 
+
+} DataQuebrada;
+
+typedef struct Qtd
+{
+    int qtdDias;
+    int qtdMeses;
+    int qtdAnos;
+    int retorno;
+
+} DiasMesesAnos;
+
+int somar(int x, int y); //função utilizada para testes
+int fatorial(int x); //função utilizada para testes
+int q1(char data[]);
+DiasMesesAnos q2(char datainicial[], char datafinal[]);
+int q3(char *texto, char c, int isCaseSensitive);
+int q4(char *strTexto, char *strBusca, int posicoes[30]);
+int q5(int num);
+int q6(int numerobase, int numerobusca);
+int q7(char matriz[8][10], char palavra[5]);
+DataQuebrada quebraData(char data[]);
+
+#endif  // MAIN_H
